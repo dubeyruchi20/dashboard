4e9009ffaf01fa905eb6d4cb4dc2d54640f1123d
dubeyruchi20 <159416038+dubeyruchi20@users.noreply.github.com>
2024-02-15 13:08:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 import streamlit as st 
 import plotly.express as px
 import pandas as pd 
-import os
-import warnings
-warnings.filterwarnings('ignore')
-st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:",layout="wide")
+#import os
+#import warnings
+#warnings.filterwarnings('ignore')
+#st.set_page_config(page_title="Superstore!!!",page_icon=":bar_chart:",layout="wide")
 
 st.title(" :bar_chart :sample SuperStore EDA")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
